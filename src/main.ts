06623976165146db8b880153d14b51ad572ef0e7
shalanah <shalanahfaith@gmail.com>
2024-02-04 15:52:11 -0600

    svg download tests

--- src/main.ts
+++ src/main.ts
@@ -88,9 +88,9 @@ const iOSHtml = /*html*/ `
 const downloadTestHtml = /*html*/ `
   <h2>Download</h2>
   <p class="desc">Downloads are not supported in most in-app browsers.</p>
-  <ul><li><a id="download-text" download href="downloadTest.txt">Small .txt download</a></li></ul>`;
+  <ul><li><button id="download-svg-blob">SVG blob download</button></li><li><a download href="blob.svg">SVG static download</button></li></ul>`;
 
-const content = /*html*/ `
+const content = /*html*/ `  
   <section>
   ${statsHtml}
   ${downloadTestHtml}
@@ -101,3 +101,27 @@ const content = /*html*/ `
 `;
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = content;
+//  Svg to test on-the-fly downloading - blob from https://www.blobmaker.app/
+document.getElementById("download-svg-blob")!.addEventListener("click", () => {
+  const svg = new Blob(
+    [
+      `
+      <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+        <path
+          fill="#FF0066"
+          d="M64.5,-19.8C71.7,1.1,57.4,30.2,34.6,46.4C11.9,62.6,-19.2,65.9,-39.4,51.8C-59.7,37.7,-69.1,6.1,-60.7,-16.6C-52.3,-39.2,-26.2,-52.9,1.3,-53.3C28.7,-53.7,57.3,-40.8,64.5,-19.8Z"
+          transform="translate(100 100)"
+        />
+      </svg>`,
+    ],
+    { type: "image/svg+xml" }
+  );
+  const url = URL.createObjectURL(svg);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "blob.svg";
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+  URL.revokeObjectURL(url);
+});
