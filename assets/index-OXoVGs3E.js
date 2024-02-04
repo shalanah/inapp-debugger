a3c3aec53f33f7c169cf280cd445c1e236ac0131
shalanah <shalanahfaith@gmail.com>
2024-02-04 15:55:45 -0600

    Updates

--- assets/index-OXoVGs3E.js
+++ assets/index-OXoVGs3E.js
@@ -30,14 +30,13 @@
 `,Fn=`
   <h2>Download</h2>
   <p class="desc">Downloads are not supported in most in-app browsers.</p>
-  <ul><li><button id="download-svg-blob">SVG blob download</button></li><li><a download href="blob.svg">SVG static download</button></li></ul>`,xn=`  
+  <ul><li><button id="download-svg-blob">SVG blob download</button></li><li><a download href="blob.svg">SVG static download</a></li></ul>`,xn=`  
   <section>
   ${Sn}
   ${Fn}
   ${dt==="Apple"?`${ot}${st}`:`${st}${ot}`}
 </section>
-`;document.querySelector("#app").innerHTML=xn;document.getElementById("download-svg-blob").addEventListener("click",()=>{const i=new Blob([`
-      <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+`;document.querySelector("#app").innerHTML=xn;document.getElementById("download-svg-blob").addEventListener("click",()=>{const i=new Blob([`<svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
         <path
           fill="#FF0066"
           d="M64.5,-19.8C71.7,1.1,57.4,30.2,34.6,46.4C11.9,62.6,-19.2,65.9,-39.4,51.8C-59.7,37.7,-69.1,6.1,-60.7,-16.6C-52.3,-39.2,-26.2,-52.9,1.3,-53.3C28.7,-53.7,57.3,-40.8,64.5,-19.8Z"
