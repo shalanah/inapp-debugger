42a2759a740e6513523b707d791470cb65675273
shalanah <shalanahfaith@gmail.com>
2024-02-06 12:21:34 -0600

    update folder

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/inapp-debugger/",
+  // base: "/inapp-debugger/", no longer needed with https://inappdebugger.com
 });
