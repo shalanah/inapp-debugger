eb31f9cd4adab1d342a42d7ef9fed55a0c774371
shalanah <shalanahfaith@gmail.com>
2024-02-27 16:13:53 -0600

    more test

--- src/sections/DeviceInfo.tsx
+++ src/sections/DeviceInfo.tsx
@@ -170,7 +170,6 @@ export const DeviceInfo = () => {
     return () => clearTimeout(timer);
   }, [deviceCopy.copied]);
 
-  // testing
   console.log({ isInApp, appName, isSFSafariViewController, window: !!window });
 
   // Inapp section
