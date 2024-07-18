6d0b66cacefd197aec34efa4986e3139e66ca3f2
Paul Esch-Laurent <p@uly.io>
2024-07-18 15:56:26 -0500

    chore: vite base dir for gh pages deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "/inapp-debugger/", //no longer needed with https://inappdebugger.com
+  base: "/inapp-debugger/",
+  // TODO: needed for GH pages subdir deploy :)g
 });
