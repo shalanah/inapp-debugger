345927adeed5340e9f3cf2cb1ce8407c035c30dd
Paul Esch-Laurent <p@uly.io>
2024-07-19 11:34:27 -0500

    revert: vite base URL config change

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/inapp-debugger/",
-  // TODO: needed for GH pages subdir deploy :)g
+  // base: "/inapp-debugger/", //no longer needed with https://inappdebugger.com
 });
