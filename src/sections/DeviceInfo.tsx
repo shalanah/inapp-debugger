71da5f9a8afb254017b4ebd36d08b38fd4092b1b
shalanah <shalanahfaith@gmail.com>
2025-01-06 16:38:04 -0600

    bump version of inapp-spy

--- src/sections/DeviceInfo.tsx
+++ src/sections/DeviceInfo.tsx
@@ -340,7 +340,7 @@ export const DeviceInfo = () => {
                   textUnderlineOffset: ".2em",
                 }}
               >
-                `inapp-spy` at version 4.2.0
+                `inapp-spy` at version 4.2.1
               </a>{" "}
               to detect in-app browsers and experimentally detect
               SFSafariViewController.
